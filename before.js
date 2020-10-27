4f7b6ed769a1532883dfe64d11eace1cd8c8b457
eduardocarvalhojunior <junior.baller@hotmail.com>
2020-10-26 23:30:07 -0300

    Atualizando

--- before.js
+++ before.js
@@ -29,3 +29,42 @@ function returnHiTo(name) { //Função que recebe parametro e returna algo
 }
 
 console.log(returnHiTo('Edu'))
+
+
+
+var; let; // variaveis que armazenam valores que podem ser alterados apenas chamando pelo nome usando = e novo valor
+
+const; // variavel constante, que armazena valor que não pode ser alterada ou redefinida.
+
+// tipos primitivos // objeto de valor
+
+let nome = 'Eduardo 7718'; // string literal  
+
+let idade = 28; // number literal
+
+let estaAprovado = true; //boolean 
+
+let corselecionada = null // para redefinir uma opção no site/sistema
+
+
+
+// objeto tipo referencia // 
+
+Object; // junta varios valores ou informações em um unico objeto ou escopo
+
+let pessoa = {
+    nome: 'Eduardo 7718',
+    idade: 28,
+    estaAprovado: true,
+};
+
+console.log(pessoa);
+
+Array; // conjunto de dados que pode ser acessado por um indice que sempre começa a contar do 0 (zero)
+
+let familia = [ 26, 44, 50, 'Edu']; 
+
+console.log(familia[3]); // vai imprimir Edu // 
+
+
+function; 
