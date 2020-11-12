b3c0ea4f8d1f92cf5d2c2ac4c0f5127ad0f6797d
eduardocarvalhojunior <junior.baller@hotmail.com>
2020-11-12 00:04:07 -0300

    Atualizando

--- before.js
+++ before.js
@@ -274,3 +274,39 @@ const cores = ['vermelho', 'azul'];
 for (let indice in cores) {
     console.log(indice,cores[indice])
 }
+
+// For Of 
+
+const cores = [ 'Vermelho','Azul', 'Verde'];
+
+for(let cor of cores) {
+    console.log(cor);
+
+}
+
+
+//----------------------------------------------------------------
+
+// Escreva uma função que usa 2 numeros e retorna o maior entre eles 
+
+let valorMaior = max(12,29);
+
+console.log(valorMaior);
+
+function max (numero1, numero2) {
+    if(numero1 > numero2)
+        return numero1;
+    else return numero2;
+}
+
+//----------------------------------------------------------------
+
+// Modo simplificado -----------------------------------// ****** Aula 24
+
+let valorMaior = max(12,29);
+
+console.log(valorMaior);
+
+function max (numero1, numero2) {
+    return numero1 > numero2 ? numero1: numero2;
+}
